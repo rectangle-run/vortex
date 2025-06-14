dff05930d239efc81e1fbd29ae0405fafdb3679f
andylovescode <andytk2010@gmail.com>
2025-06-13 18:22:47 -0700

    Discovery: Integrate Discovery Rust

--- packages/wormhole/src/cli.ts
+++ packages/wormhole/src/cli.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env bun
 
 import { Lifetime } from "@vortexjs/core";
+import { compileScript } from "@vortexjs/discovery";
 import { getConfig } from "./dev/config";
 import { createErrorCollector } from "./dev/markup";
 import { developmentServer } from "./dev/server";
 import { taskBoard } from "./dev/tasks";
-import { discovery_compile, discovery_debug, discovery_new } from "./discovery";
 
 const lt = new Lifetime();
 const config = await getConfig(lt, process.cwd());
@@ -15,9 +15,7 @@ developmentServer(lt, config);
 
 const errorCollector = createErrorCollector();
 
-const discovery = discovery_new({
-	filename: "test",
-	source: `import test from "@vortexjs/wormhole/route";
+const { discoveries, source } = compileScript(`import test from "@vortexjs/wormhole/route";
 	test("/", {
 		page() {
 			return (
@@ -27,10 +25,6 @@ const discovery = discovery_new({
 				</>
 			);
 		},
-	});`,
-	errors: errorCollector,
-});
+	});`, "text.tsx");
 
-await discovery_compile(discovery);
-
-discovery_debug(discovery);
+console.log({ discoveries, source });
--- packages/wormhole/src/performance/cached.ts
+++ packages/wormhole/src/performance/cached.ts
@@ -1,62 +1,62 @@
-import { paths } from "../paths";
-import { unwrap } from "../utils";
+import { paths } from "../dev/paths";
+import { unwrap } from "../shared/utils";
 
 export interface Cache<This, Args extends any[], Return> {
-	(this: This, ...args: Args): Promise<Return>;
-	disk(cacheNamespace: string): Cache<This, Args, Return>;
+    (this: This, ...args: Args): Promise<Return>;
+    disk(cacheNamespace: string): Cache<This, Args, Return>;
 }
 
 export function cached<This, Args extends any[], Return>(
-	target: (this: This, ...args: Args) => Return,
+    target: (this: This, ...args: Args) => Return,
 ): Cache<This, Args, Return> {
-	const cache = new Map<string, Return>();
-	let diskNamespace: string | undefined = undefined;
+    const cache = new Map<string, Return>();
+    let diskNamespace: string | undefined = undefined;
 
-	const result = async function (this: This, ...args: Args): Promise<Return> {
-		const key = JSON.stringify(args);
+    const result = async function (this: This, ...args: Args): Promise<Return> {
+        const key = JSON.stringify(args);
 
-		if (cache.has(key)) {
-			return unwrap(cache.get(key));
-		}
+        if (cache.has(key)) {
+            return unwrap(cache.get(key));
+        }
 
-		// check disk cache
-		if (diskNamespace) {
-			const hashedKey = Bun.hash(key).toString(16);
-			const path = paths()
-				.wormhole.cache.namespace(diskNamespace)
-				.eval(hashedKey);
+        // check disk cache
+        if (diskNamespace) {
+            const hashedKey = Bun.hash(key).toString(16);
+            const path = paths()
+                .wormhole.cache.namespace(diskNamespace)
+                .eval(hashedKey);
 
-			try {
-				const contents = (await Bun.file(path).json()) as Return;
-				cache.set(key, contents);
-				return contents;
-			} catch {}
-		}
+            try {
+                const contents = (await Bun.file(path).json()) as Return;
+                cache.set(key, contents);
+                return contents;
+            } catch { }
+        }
 
-		const result = await target.call(this, ...args);
+        const result = await target.call(this, ...args);
 
-		cache.set(key, result);
+        cache.set(key, result);
 
-		if (diskNamespace) {
-			async function saveToDisk() {
-				const hashedKey = Bun.hash(key).toString(16);
-				const path = paths()
-					.wormhole.cache.namespace(unwrap(diskNamespace))
-					.eval(hashedKey);
-				await Bun.write(path, JSON.stringify(result, null, "\t"));
-			}
+        if (diskNamespace) {
+            async function saveToDisk() {
+                const hashedKey = Bun.hash(key).toString(16);
+                const path = paths()
+                    .wormhole.cache.namespace(unwrap(diskNamespace))
+                    .eval(hashedKey);
+                await Bun.write(path, JSON.stringify(result, null, "\t"));
+            }
 
-			saveToDisk();
-		}
+            saveToDisk();
+        }
 
-		return result;
-	} as Cache<This, Args, Return>;
+        return result;
+    } as Cache<This, Args, Return>;
 
-	result.disk = (cacheNamespace: string): Cache<This, Args, Return> => {
-		diskNamespace = cacheNamespace;
+    result.disk = (cacheNamespace: string): Cache<This, Args, Return> => {
+        diskNamespace = cacheNamespace;
 
-		return result;
-	};
+        return result;
+    };
 
-	return result;
+    return result;
 }
