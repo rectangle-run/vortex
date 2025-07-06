76cba721e0cd1d8642acaeba3fe82a6277716d26
andylovescode <andytk2010@gmail.com>
2025-07-05 19:20:17 -0700

    Meta: Unify package versions w/ `cataloger`

--- packages/cataloger/src/index.ts
+++ packages/cataloger/src/index.ts
@@ -6,11 +6,11 @@ import { join } from "node:path/posix";
 import process from "node:process";
 
 const confirmation = await confirm({
-	message: "Are you sure you want to continue? Cataloger is in beta, so you should make sure you have backups of your data.",
+    message: "Are you sure you want to continue? Cataloger is in beta, so you should make sure you have backups of your data.",
 });
 
 if (!confirmation) {
-	process.exit(0);
+    process.exit(0);
 }
 
 const root = await findTopLevelProject(process.cwd());
@@ -22,9 +22,9 @@ const glob = new Bun.Glob("**/package.json");
 const packageJsons: string[] = [];
 
 for await (const pkg of glob.scan({ cwd: root, absolute: true })) {
-	if (!pkg.includes("node_modules") && pkg !== join(root, "package.json")) {
-		packageJsons.push(pkg);
-	}
+    if (!pkg.includes("node_modules") && pkg !== join(root, "package.json")) {
+        packageJsons.push(pkg);
+    }
 }
 
 const collectedVersions: Record<string, string[]> = {};
@@ -32,81 +32,81 @@ const collectedVersions: Record<string, string[]> = {};
 type DependencySet = Record<string, string>;
 
 type PackageJson = {
-	dependencies?: DependencySet;
-	devDependencies?: DependencySet;
-	peerDependencies?: DependencySet;
-	optionalDependencies?: DependencySet;
-	workspaces?: {
-		catalog?: Record<string, string>;
-	}
+    dependencies?: DependencySet;
+    devDependencies?: DependencySet;
+    peerDependencies?: DependencySet;
+    optionalDependencies?: DependencySet;
+    workspaces?: {
+        catalog?: Record<string, string>;
+    }
 };
 
 async function collectVersions(pkgPath: string) {
-	const pkg: PackageJson = await Bun.file(pkgPath).json();
+    const pkg: PackageJson = await Bun.file(pkgPath).json();
 
-	const deps: DependencySet[] = [
-		pkg.dependencies,
-		pkg.devDependencies,
-		pkg.peerDependencies,
-		pkg.optionalDependencies,
-	].filter(x => x !== undefined);
+    const deps: DependencySet[] = [
+        pkg.dependencies,
+        pkg.devDependencies,
+        pkg.peerDependencies,
+        pkg.optionalDependencies,
+    ].filter(x => x !== undefined);
 
-	const unifiedDeps: Record<string, string> = {};
+    const unifiedDeps: Record<string, string> = {};
 
-	for (const dep of deps) {
-		for (const [name, version] of Object.entries(dep)) {
-			unifiedDeps[name] = version;
-		}
-	}
+    for (const dep of deps) {
+        for (const [name, version] of Object.entries(dep)) {
+            unifiedDeps[name] = version;
+        }
+    }
 
-	for (const name in unifiedDeps) {
-		if (!collectedVersions[name]) {
-			collectedVersions[name] = [];
-		}
-		collectedVersions[name].push(unwrap(unifiedDeps[name]));
-	}
+    for (const name in unifiedDeps) {
+        if (!collectedVersions[name]) {
+            collectedVersions[name] = [];
+        }
+        collectedVersions[name].push(unwrap(unifiedDeps[name]));
+    }
 }
 
 await Promise.all(
-	packageJsons.map((pkgPath) => {
-		return collectVersions(pkgPath);
-	}),
+    packageJsons.map((pkgPath) => {
+        return collectVersions(pkgPath);
+    }),
 );
 
 const resolvedVersions: Record<string, string> = {};
 
 function isWorkspace(version: string): boolean {
-	return version.startsWith("workspace:");
+    return version.startsWith("workspace:");
 }
 
 function isCatalog(version: string): boolean {
-	return version.startsWith("catalog:");
+    return version.startsWith("catalog:");
 }
 
 for (const name in collectedVersions) {
-	const uniqueVersions = new Set(unwrap(collectedVersions[name]).filter(x => !isCatalog(x)));
+    const uniqueVersions = new Set(unwrap(collectedVersions[name]).filter(x => !isCatalog(x)));
 
-	if (uniqueVersions.size === 1) {
-		const version = unwrap(uniqueVersions.values().next().value);
+    if (uniqueVersions.size === 1) {
+        const version = unwrap(uniqueVersions.values().next().value);
 
-		if (!isWorkspace(version)) {
-			resolvedVersions[name] = version;
-		}
-	} else {
-		const opts = [...uniqueVersions];
+        if (!isWorkspace(version)) {
+            resolvedVersions[name] = version;
+        }
+    } else if (uniqueVersions.size > 1) {
+        const opts = [...uniqueVersions];
 
-		const selected = await select({
-			message: `Multiple different versions are used for ${name}, which one should be definitive`,
-			choices: opts.map((version) => ({
-				name: version,
-				value: version,
-			})),
-		})
+        const selected = await select({
+            message: `Multiple different versions are used for ${name}, which one should be definitive`,
+            choices: opts.map((version) => ({
+                name: version,
+                value: version,
+            })),
+        })
 
-		if (!isWorkspace(selected)) {
-			resolvedVersions[name] = selected;
-		}
-	}
+        if (!isWorkspace(selected)) {
+            resolvedVersions[name] = selected;
+        }
+    }
 }
 
 const rootPkg: PackageJson = await Bun.file(join(root, "package.json")).json();
@@ -115,37 +115,37 @@ rootPkg.workspaces ??= {};
 
 rootPkg.workspaces.catalog ??= {};
 rootPkg.workspaces.catalog = {
-	...rootPkg.workspaces.catalog,
-	...resolvedVersions
+    ...rootPkg.workspaces.catalog,
+    ...resolvedVersions
 }
 
 await Bun.write(join(root, "package.json"), JSON.stringify(rootPkg, null, "\t"));
 
 async function upgradePackageJson(pkgPath: string) {
-	const pkg: PackageJson = await Bun.file(pkgPath).json();
+    const pkg: PackageJson = await Bun.file(pkgPath).json();
 
-	const deps: DependencySet[] = [
-		pkg.dependencies,
-		pkg.devDependencies,
-		pkg.peerDependencies,
-		pkg.optionalDependencies,
-	].filter(x => x !== undefined);
+    const deps: DependencySet[] = [
+        pkg.dependencies,
+        pkg.devDependencies,
+        pkg.peerDependencies,
+        pkg.optionalDependencies,
+    ].filter(x => x !== undefined);
 
-	for (const dep of deps) {
-		for (const name in dep) {
-			if (resolvedVersions[name]) {
-				dep[name] = "catalog:";
-			}
-		}
-	}
+    for (const dep of deps) {
+        for (const name in dep) {
+            if (resolvedVersions[name]) {
+                dep[name] = "catalog:";
+            }
+        }
+    }
 
-	await Bun.write(pkgPath, JSON.stringify(pkg, null, "\t"));
+    await Bun.write(pkgPath, JSON.stringify(pkg, null, "\t"));
 }
 
 await Promise.all(
-	packageJsons.map((pkgPath) => {
-		return upgradePackageJson(pkgPath);
-	}),
+    packageJsons.map((pkgPath) => {
+        return upgradePackageJson(pkgPath);
+    }),
 );
 
 console.log("All package.json files have been updated with the resolved versions.");
