cf5c4064a9fac7522e549c562e90070f37fbe81b
andylovescode <andytk2010@gmail.com>
2025-07-31 11:07:26 -0700

    Core: Improve awaited tying

---
+++ .changeset/rotten-things-cut.md
@@ -0,0 +1,5 @@
+---
+"@vortexjs/core": patch
+---
+
+Improve awaited typing
--- packages/vortex-core/src/std/awaited.ts
+++ packages/vortex-core/src/std/awaited.ts
@@ -1,6 +1,6 @@
 import { type Signal, useState } from "../signal";
 
-export function awaited<T>(value: T | Promise<T>): Signal<T | undefined> {
+export function awaited<T>(value: Promise<T>): Signal<T | undefined> {
 	const result = useState<T | undefined>(undefined);
 
 	async function fetchValue() {
