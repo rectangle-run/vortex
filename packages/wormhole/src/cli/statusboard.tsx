bb769809c38840b3d0d58a3ef33beaabd56334d0
andylovescode <andytk2010@gmail.com>
2025-08-18 00:04:36 -0700

    Wormhole: Add custom logger

--- packages/wormhole/src/cli/statusboard.tsx
+++ packages/wormhole/src/cli/statusboard.tsx
@@ -11,9 +11,10 @@ import {
 } from "@vortexjs/core";
 import type { Project } from "~/state";
 import { getImmediateValue, type Store, useState } from "@vortexjs/core";
-import { cliApp } from "@vortexjs/cli";
+import { cliApp, colors } from "@vortexjs/cli";
 import { Frame, Text } from "@vortexjs/intrinsics";
 import { theme } from "./theme";
+import type { HTTPMethod } from "~/shared/http-method";
 
 function Throbber() {
 	let i = store(0);
@@ -44,6 +45,156 @@ export function StatusBoard(state: Project) {
 	const lt = state.lt;
 	using _hlt = Lifetime.changeHookLifetime(lt);
 
+	cliApp(<Frame width="100%" height="100%" direction="row" alignItems="stretch">
+		<LeftPanel />
+		<LogPanel />
+	</Frame>)
+}
+
+function Prefix({ text, color }: { text: string; color: string }) {
+	const width = 6;
+
+	return <Text color={color} weight="bold">
+		{text.padEnd(width, " ")}
+	</Text>
+}
+
+function getMethodColor(method: HTTPMethod): string {
+	switch (method) {
+		case "GET":
+			return colors.blue[400];
+		case "POST":
+			return colors.emerald[400];
+		case "DELETE":
+			return colors.red[400];
+		case "PATCH":
+			return colors.yellow[400];
+	}
+}
+
+function StatusCode({ code }: { code: number }) {
+	let accent = colors.gray[400];
+
+	if (code >= 400 && code < 500) {
+		accent = colors.rose[400];
+	}
+	if (code >= 500) {
+		accent = colors.red[400];
+	}
+	if (code >= 300 && code < 400) {
+		accent = colors.emerald[400];
+	}
+
+	return <Text color={accent} weight="bold">
+		{code.toString().padStart(3, "0")}
+	</Text>
+}
+
+function NetworkTag({ tag }: { tag: RequestTag }) {
+	let color: string;
+
+	switch (tag) {
+		case "api":
+			color = colors.blue[400];
+			break;
+		case "static":
+			color = colors.emerald[400];
+			break;
+		case "ssr":
+			color = colors.yellow[400];
+			break;
+		case "query":
+			color = colors.purple[400];
+			break;
+		case "mutation":
+			color = colors.orange[400];
+			break;
+		default:
+			color = colors.gray[400];
+	}
+
+	return <Text color={color}>
+		{" "}({tag})
+	</Text>;
+}
+
+function LogView({ log }: { log: Log }) {
+	if (log.type === "raw") {
+		return <Text color={log.color ?? theme.textMuted}>
+			{log.message}
+		</Text>;
+	} else if (log.type === "request") {
+		const urlLength = 25;
+
+		return <Text color={theme.text}>
+			<Prefix text={log.method} color={getMethodColor(log.method)} /> {log.url.padEnd(urlLength)} <StatusCode code={log.responseCode} />
+			{list(log.tags).show(tag => <NetworkTag tag={tag} />)}
+		</Text>;
+	}
+	return <></>;
+}
+
+function LogPanel() {
+	enableConsoleLogShim();
+
+	return <Frame grow={1} background={theme.backgroundRaised} padding={16} direction="column" gap={2}>
+		<Text color={theme.text}>Logger</Text>
+		<Frame width="100%" grow={1} clip>
+			<Frame position="absolute" left={0} bottom={0} right={0} minHeight="100%" gap={2} direction="column">
+				{list(logs).show(log => <LogView log={log} />)}
+			</Frame>
+		</Frame>
+	</Frame>
+}
+
+export interface Task {
+	[Symbol.dispose]: () => void;
+	name: string;
+}
+
+export type RequestTag = "api" | "static" | "ssr" | "query" | "mutation";
+
+export type Log = {
+	type: "raw";
+	message: string;
+	color?: string;
+} | {
+	type: "request";
+	method: HTTPMethod;
+	url: string;
+	responseCode: number;
+	tags: RequestTag[];
+}
+
+export const tasks: Store<Task[]> = useState([]);
+export const logs: Store<Log[]> = useState([]);
+
+export function addLog(log: Log) {
+	logs.set([...getImmediateValue(logs), log]);
+}
+
+function enableConsoleLogShim() {
+	const formatArgs = (args: any[]) => {
+		return args.map(x => typeof x === "string" ? x : Bun.inspect(x)).join(" ")
+	}
+
+	console.log = (...args: any[]) => {
+		const formatted = formatArgs(args);
+		addLog({ type: "raw", message: formatted });
+	}
+
+	console.error = (...args: any[]) => {
+		const formatted = formatArgs(args);
+		addLog({ type: "raw", message: formatted, color: colors.red[400] });
+	}
+
+	console.warn = (...args: any[]) => {
+		const formatted = formatArgs(args);
+		addLog({ type: "raw", message: formatted, color: colors.yellow[400] });
+	}
+}
+
+function LeftPanel() {
 	const uptime = useState("uptime");
 	let starting = Date.now();
 
@@ -64,40 +215,28 @@ export function StatusBoard(state: Project) {
 		);
 	}, 100);
 
-	cliApp(<Frame width="100%" height="100%" direction="row" alignItems="stretch">
-		<Frame grow={1} background={theme.background} padding={16} direction="column" gap={2}>
-			<Text color={theme.text}>
-				<Text color={theme.accent}>▐</Text>
-				<Text background={theme.accent} color="black">
-					•
-				</Text>
-				<Text color={theme.accent}>▌</Text> wormhole
+	return <Frame grow={1} background={theme.background} padding={16} direction="column" gap={2}>
+		<Text color={theme.text}>
+			<Text color={theme.accent}>▐</Text>
+			<Text background={theme.accent} color="black">
+				•
 			</Text>
-			<Text color={theme.textMuted}>
-				{uptime}
-			</Text>
-			<Frame width="100%" border={theme.border} padding={8} direction="column" gap={2}>
-				<Frame padding={{ x: 2 }} position="absolute" left={2} top={-4} background={theme.background}>
-					<Text color={theme.textMuted}>Tasks</Text>
-				</Frame>
-
-				{list(tasks).show((item) => (
-					<TaskView task={item} />
-				))}
+			<Text color={theme.accent}>▌</Text> wormhole
+		</Text>
+		<Text color={theme.textMuted}>
+			{uptime}
+		</Text>
+		<Frame width="100%" border={theme.border} padding={8} direction="column" gap={2}>
+			<Frame padding={{ x: 2 }} position="absolute" left={2} top={-4} background={theme.background}>
+				<Text color={theme.textMuted}>Tasks</Text>
 			</Frame>
-		</Frame>
-		<Frame grow={1} background={theme.backgroundRaised} padding={16} direction="column" gap={2}>
-			<Text color={theme.text}>Errors</Text>
-		</Frame>
-	</Frame>)
-}
 
-export interface Task {
-	[Symbol.dispose]: () => void;
-	name: string;
+			{list(tasks).show((item) => (
+				<TaskView task={item} />
+			))}
+		</Frame>
+	</Frame>;
 }
-
-export const tasks: Store<Task[]> = useState([]);
 
 export function addTask(props: Omit<Task, typeof Symbol.dispose>): Task {
 	const task: Task = {
